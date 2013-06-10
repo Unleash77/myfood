e29377c91a19aa8a3f81f25a03252d425973f6f6
Dawid Warta <wartadawid@gmail.com>
2013-06-10 19:47:51 +0200

    adding elements

--- scripts.js
+++ scripts.js
@@ -12,13 +12,13 @@ function create_db() {
             transaction.executeSql (sql, undefined);
         });		
         
-        var sql2 = " CREATE TABLE purposes " +
-                " (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
-                "name VARCHAR(100) NOT NULL); ";
-            
         db.transaction (function (transaction) 
         {
-            transaction.executeSql (sql2, undefined);
+            var sql2 = "INSERT INTO types (name) VALUES (?)";
+
+            transaction.executeSql (sql2, ["Nabiał"]);
+            transaction.executeSql (sql2, ["Mięso"]);
+            transaction.executeSql (sql2, ["Napój"]);
         });
         
         var sql3 = "CREATE TABLE products " +
@@ -31,8 +31,18 @@ function create_db() {
                 "calories INTEGER(10) NOT NULL, " + 
                 "proteins INTEGER(10) NOT NULL, " + 
                 "carbohydrates INTEGER(10) NOT NULL, " + 
-                "fats INTEGER(10) NOT NULL); ";
-            
+                "fats INTEGER(10) NOT NULL, " +
+                "taste_1 INTEGER(1) NOT NULL, " + 
+                "taste_2 INTEGER(1) NOT NULL, " + 
+                "taste_3 INTEGER(1) NOT NULL, " + 
+                "taste_4 INTEGER(1) NOT NULL, " + 
+                "taste_5 INTEGER(1) NOT NULL, " + 
+                "meal_1 INTEGER(1) NOT NULL, " + 
+                "meal_2 INTEGER(1) NOT NULL, " + 
+                "meal_3 INTEGER(1) NOT NULL, " + 
+                "meal_4 INTEGER(1) NOT NULL, " + 
+                "meal_5 INTEGER(1) NOT NULL); ";
+                
         db.transaction (function (transaction) 
         {
             transaction.executeSql (sql3, undefined);
@@ -58,7 +68,7 @@ function delete_db() {
     });
 }
 
-function add_to_products(name,count,type,deadline_date,calories,proteins,carbohydrates,fats) {
+function add_to_products(name,count,type,deadline_date,calories,proteins,carbohydrates,fats, taste_1, taste_2, taste_3, taste_4, taste_5, meal_1, meal_2, meal_3, meal_4, meal_5) {
     var currentTime = new Date();
     var month = currentTime.getMonth() + 1;
     if (month < 10) {
@@ -70,10 +80,46 @@ function add_to_products(name,count,type,deadline_date,calories,proteins,carbohy
     
     db.transaction (function (transaction) 
     {
-        var sql = "INSERT INTO products (name, type_id, creation_date, deadline_date, count, calories, proteins, carbohydrates, fats ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        transaction.executeSql (sql, [name, type, today, deadline_date, count, calories, proteins, carbohydrates, fats], function ()
+        var sql = "INSERT INTO products (name, type_id, creation_date, deadline_date, count, calories, proteins, carbohydrates, fats, taste_1, taste_2, taste_3, taste_4, taste_5, meal_1, meal_2, meal_3, meal_4, meal_5 ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,?)";
+        transaction.executeSql (sql, [name, type, today, deadline_date, count, calories, proteins, carbohydrates, fats, taste_1, taste_2, taste_3, taste_4, taste_5, meal_1, meal_2, meal_3, meal_4, meal_5], function ()
             { 
                 alert('dodano!');
             }, error);
     });
+}
+
+function add_to_type(name) {
+    db.transaction (function (transaction) 
+    {
+        var sql = "INSERT INTO types (name) VALUES (?)";
+        transaction.executeSql (sql, [name], function ()
+            { 
+                alert('dodano!');
+            }, error);
+    });
+}
+
+function get_types() {
+    
+    db.transaction (function (transaction) 
+    {
+        var sql = "SELECT * FROM types";
+        transaction.executeSql (sql, undefined, 
+        function (transaction, result)
+        {
+          var html = '';
+          if (result.rows.length)
+          {
+                for (var i = 0; i < result.rows.length; i++) 
+                {
+                  var row = result.rows.item (i);
+                  var name = row.name;
+                  var id = row.id;
+                  html += '<option value="'+id+'">'+name+'</option>';
+                }
+          }
+          $('select[name="selecttype"]').html(html).selectmenu('refresh', true);
+        });
+    });
+    
 }
